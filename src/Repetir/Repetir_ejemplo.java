8742ae6333f4329b802535b6316c33db4b649925
Alumno <Alumno@LAPTOP-ANC4HNVR>
2020-02-13 22:14:29 -0300

    uno

---
+++ src/Repetir/Repetir_ejemplo.java
@@ -0,0 +1,17 @@
+package Repetir;
+
+public class Repetir_ejemplo {
+
+	public static void main(String[] args) {
+		
+	System.out.println("hola");
+			 
+		       int i=0; 
+		    		   while (true) {
+		    			   i++;
+		    			   System.out.println("Valor de i: " +i );
+		    			   if(i==9)break;}
+
+	}
+
+}
